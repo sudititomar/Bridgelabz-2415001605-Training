9907f76b3ae75e6601cb5d57899ccb1e8e805fb3
sudititomar <sudytyytomar@gmail.com>
2025-08-20 21:30:12 +0530

    Update avgof3numbers.java

---
+++ practice_problem/avgof3numbers.java
@@ -0,0 +1,11 @@
+class avgof3numbers
+{
+    public static void main(String[] args)
+    {
+        double num1 = 10;
+        double num2 = 20;
+        double num3 = 30;
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("Average of 3 Numbers: " + average);
+    }
+}
