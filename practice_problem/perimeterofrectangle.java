325228226f62745e9380b2f689f020808eeb9046
sudititomar <sudytyytomar@gmail.com>
2025-08-20 21:30:51 +0530

    Update perimeterofrectangle.java

---
+++ practice_problem/perimeterofrectangle.java
@@ -0,0 +1,10 @@
+perimeterofrectangle
+{
+    public static void main(String[] args)
+    {
+        double length = 5;
+        double width = 3;
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter of Rectangle: " + perimeter);
+    }
+}
