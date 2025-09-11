02357252c78f534b1cf08ae033e67c376f476008
sudititomar <sudytyytomar@gmail.com>
2025-09-11 22:26:30 +0530

    Add files via upload

---
+++ OOPS Practice Questions/Level 2/Student.java
@@ -0,0 +1,41 @@
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    
+    String calculateGrade() {
+        if (marks >= 90) {
+            return "A+";
+        } else if (marks >= 80) {
+            return "A";
+        } else if (marks >= 70) {
+            return "B";
+        } else if (marks >= 60) {
+            return "C";
+        } else if (marks >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+
+    void displayStudentDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Suditi", 101, 85.5);
+        student1.displayStudentDetails();
+    }
+}
