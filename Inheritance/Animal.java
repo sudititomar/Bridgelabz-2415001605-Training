84f0d96cfcb5d8a9ac4a966086b413d44df44f8b
Suditi Tomar <sudytyytomar@gmail.com>
2025-10-13 13:31:49 +0530

    Animal.java

---
+++ Inheritance/Animal.java
@@ -0,0 +1,58 @@
+class Animal {
+    String name;
+    int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println(name + " (Age: " + age + ") makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " (Dog, Age: " + age + "): Woof Woof!");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " (Cat, Age: " + age + "): Meow Meow!");
+    }
+}
+
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " (Bird, Age: " + age + "): Tweet Tweet!");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Buddy", 3);
+        Animal cat = new Cat("Whiskers", 2);
+        Animal bird = new Bird("Tweety", 1);
+
+        dog.makeSound();
+        cat.makeSound();
+        bird.makeSound();
+    }
+}
